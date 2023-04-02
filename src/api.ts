b7e9cb41f4da5c46b69e08ddf43284f66e8c702c
ninh-nd <ninh.nd194815@sis.hust.edu.vn>
2023-04-02 21:38:22 +0700

    Update API base url

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:3000/api";
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: process.env.API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
